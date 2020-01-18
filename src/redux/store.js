e828caabab2015ca3e774b118ec5661ef2194cc6
mrrobot16 <hmgaar@gmail.com>
2020-01-18 15:57:54 -0500

    Added redux folder to project

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,7 @@ import { createStore, applyMiddleware, compose } from 'redux'
 import { connectRouter, routerMiddleware } from 'connected-react-router'
 import thunk from 'redux-thunk'
 import * as History from 'history'
-import rootReducer from './modules'
+import rootReducer from './reducer'
 
 export const history = History.createBrowserHistory()
 
