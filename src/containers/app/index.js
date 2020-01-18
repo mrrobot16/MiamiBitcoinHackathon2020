07af9c97d3bbe8f13e8b67d4de478e742239607b
mrrobot16 <hmgaar@gmail.com>
2020-01-18 12:10:21 -0500

    Minor changes

--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -1,11 +1,18 @@
 import React from 'react'
-import { Route } from 'react-router-dom'
-import Home from '../home'
+import { Route, Link  } from 'react-router-dom'
+import Home from 'containers/home'
+import Address from 'containers/address'
 
 const App = () => (
   <div>
+    <header>
+      <Link to="/">Home</Link>
+      <Link to="/address/1234">Address</Link>
+    </header>
+
     <main>
       <Route exact path="/" component={Home} />
+      <Route exact path="/address/:address" component={Address} />
     </main>
   </div>
 )
