d7b0d65e20c1c65c6442d0fce2eea54c673a0575
mrrobot16 <hmgaar@gmail.com>
2020-01-18 20:47:05 -0500

    Added endpoints constant file

---
+++ src/constants/endpoints.js
@@ -0,0 +1,6 @@
+const baseURL = 'https://arcane-lowlands-09967.herokuapp.com'
+export const endpoints = {
+  address: baseURL + '/address',
+  transactions: baseURL + '/transactions',
+  wallets: baseURL + '/wallets'
+}
--- src/utils/http.js
+++ src/utils/http.js
@@ -1,3 +1,20 @@
-export const get = (url) => {
-  
+export const get = async (url) => {
+  const logData = data => {
+    console.log('data', data);
+  }
+  const data = await fetch(url)
+  logData(data)
+}
+
+export const post = async (url, data) => {
+  data = {
+    method: 'POST',
+    mode: 'cors',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  }
+  const response = await fetch(url, data)
+  return await response.json()
 }
